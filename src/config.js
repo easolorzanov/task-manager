2b07657bfe448b73102f9c03f4aa69514f2da08e
easolorzanov <easolorzanov21@gmail.com>
2023-10-23 20:34:24 -0500

    deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "https://tasks-mongooo.mongo.cosmos.azure.com:443/";
+  process.env.MONGODB_URI || "mongodb://tasks-mongooo:RGvtLFdVbYdIaRlEZRfgUeCA6ZedxMQugNyJk8oskrawShfyx2HLxxvNGoZeWTel0RrS29xW9vbFACDbAxZa9g==@tasks-mongooo.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@tasks-mongooo@";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
